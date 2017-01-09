3dff1523be876bdd8cf1ce38a01cccd3ac3b8546
Lerrel Pinto <lerrel.pinto@gmail.com>
2017-01-09 12:31:03 -0500

    Visualization

--- adversary_scripts/train_trpo_adversary.py
+++ adversary_scripts/train_trpo_adversary.py
@@ -160,37 +160,13 @@ for ne in range(n_exps):
     rand_test_rew_summary.append(rand_testing_rews)
     adv_test_rew_summary.append(adv_testing_rews)
 
-## PRINTING ##
-import matplotlib.pyplot as plt
-con_rew = np.array(const_test_rew_summary)
-ran_rew = np.array(rand_test_rew_summary)
-adv_rew = np.array(adv_test_rew_summary)
-mean_con = con_rew.mean(0)
-std_con = con_rew.std(0)
-mean_ran = ran_rew.mean(0)
-std_ran = ran_rew.std(0)
-mean_adv = adv_rew.mean(0)
-std_adv = adv_rew.std(0)
-
-x = [i for i in range(len(mean_con))]
-plt.plot(x,mean_con,color=(1.,0.,0.), linewidth=2.0)
-plt.fill_between(x, mean_con-std_con, mean_con+std_con,color=(0.5,0.1,0.1), alpha=0.5)
-plt.plot(x,mean_ran,color=(0.,1.,0.), linewidth=2.0)
-plt.fill_between(x, mean_ran-std_ran, mean_ran+std_ran,color=(0.1,0.5,0.1), alpha=0.5)
-plt.plot(x,mean_adv,color=(0.,0.,1.), linewidth=2.0)
-plt.fill_between(x, mean_adv-std_adv, mean_adv+std_adv,color=(0.1,0.1,0.5), alpha=0.5)
-import matplotlib.patches as mpatches
-
-red_patch = mpatches.Patch(color=(1.,0.,0.), label='Testing with 0 adversary')
-green_patch = mpatches.Patch(color=(0.,1.,0.), label='Testing with random adversary')
-blue_patch = mpatches.Patch(color=(0.,0.,1.), label='Testing with learnt adversary')
-plt.legend(handles=[red_patch,green_patch,blue_patch])
-axes = plt.gca()
-axes.set_ylim([0,4000])
-plt.title(save_name)
-pickle.dump({'zero_test': const_test_rew_summary,
+## SAVING INFO ##
+pickle.dump({'args': args,
+             'pro_policy': pro_policy,
+             'adv_policy': adv_policy,
+             'zero_test': const_test_rew_summary,
              'rand_test': rand_test_rew_summary,
              'adv_test': adv_test_rew_summary}, open(save_name,'wb'))
-plt.savefig(fig_name)
-#plt.show()
+
+logger.log('\n\n\n#### DONE ####\n\n\n')
 embed()
---
+++ adversary_scripts/viz_results.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pickle
+
+import argparse
+
+## Pass arguments ##
+parser = argparse.ArgumentParser()
+parser.add_argument('r', type=str, help='filepath to results')
+
+args = parser.parse_args()
+savename = args.r
+
+res_D = pickle.load(open(savename,'rb'))
+const_test_rew_summary = res_D['zero_test']
+rand_test_rew_summary = res_D['rand_test']
+adv_test_rew_summary = res_D['adv_test']
+
+## PRINTING ##
+con_rew = np.array(const_test_rew_summary)
+ran_rew = np.array(rand_test_rew_summary)
+adv_rew = np.array(adv_test_rew_summary)
+mean_con = con_rew.mean(0)
+std_con = con_rew.std(0)
+mean_ran = ran_rew.mean(0)
+std_ran = ran_rew.std(0)
+mean_adv = adv_rew.mean(0)
+std_adv = adv_rew.std(0)
+
+x = [i for i in range(len(mean_con))]
+plt.plot(x,mean_con,color=(1.,0.,0.), linewidth=2.0)
+plt.fill_between(x, mean_con-std_con, mean_con+std_con,color=(0.5,0.1,0.1), alpha=0.5)
+plt.plot(x,mean_ran,color=(0.,1.,0.), linewidth=2.0)
+plt.fill_between(x, mean_ran-std_ran, mean_ran+std_ran,color=(0.1,0.5,0.1), alpha=0.5)
+plt.plot(x,mean_adv,color=(0.,0.,1.), linewidth=2.0)
+plt.fill_between(x, mean_adv-std_adv, mean_adv+std_adv,color=(0.1,0.1,0.5), alpha=0.5)
+
+red_patch = mpatches.Patch(color=(1.,0.,0.), label='Testing with 0 adversary')
+green_patch = mpatches.Patch(color=(0.,1.,0.), label='Testing with random adversary')
+blue_patch = mpatches.Patch(color=(0.,0.,1.), label='Testing with learnt adversary')
+plt.legend(handles=[red_patch,green_patch,blue_patch])
+axes = plt.gca()
+#axes.set_ylim([0,4000])
+plt.title(savename)
+plt.show()
