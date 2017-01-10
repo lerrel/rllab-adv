36a9b5aa0f211f3402434b018c40121668c9e549
lerrelpinto <lerrelp@yoda.graphics.cs.cmu.edu>
2017-01-10 23:45:01 +0000

    cleanup

--- adversary_scripts/yoda_rl_driver.sh
+++ adversary_scripts/yoda_rl_driver.sh
@@ -22,6 +22,6 @@ OUTPUT_FILER=/home/${USER}/tmpoutputs/${PROCSTRING_BASENAME}.${HOSTNAME}.$$.outp
 cd /home/lerrelp/rllab/adversary_scripts
 
 #run a niced python script 
-nice python train_trpo_var_adversary.py --env $env --adv_name adv --n_exps 3 --n_itr 500 --layer_size 64 64 --batch_size 25000 --if_render 0 --adv_fraction $adv_fraction
+nice python train_trpo_var_adversary.py --env $env --adv_name adv --n_exps 3 --n_itr 500 --layer_size 64 64 --batch_size 25000 --if_render 0 --adv_fraction $adv_fraction > $OUTPUT_FILER
 echo "Finished Without Problems" >> $OUTPUT_FILER
 echo "..::RL Solved::.."
--- adversary_scripts/yoda_rl_starter.sh
+++ adversary_scripts/yoda_rl_starter.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+LOGDIR=/home/${USER}/tmpoutputs/
+if [ ! -d ${LOGDIR} ]; then
+    echo "Directory ${LOGDIR} not present, creating it"
+    mkdir $LOGDIR
+fi
+
+LOGSTRING="-e ${LOGDIR} -o ${LOGDIR} -j oe"
+
 for env in "HopperAdv-v1" "HalfCheetahAdv-v1" "Walker2dAdv-v1";
 do
     for adv_fraction in "0.1" "0.25" "0.5" "1.0" "2.0" "5.0"  "10.0";
