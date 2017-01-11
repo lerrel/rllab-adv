93a13f8fdcc56669aae628da8a5c183eafb3496b
lerrelpinto <lerrelp@yoda.graphics.cs.cmu.edu>
2017-01-11 01:07:24 +0000

    .

--- adversary_scripts/yoda_rl_no_adv_driver.sh
+++ adversary_scripts/yoda_rl_no_adv_driver.sh
@@ -17,11 +17,11 @@ if [ ! -n "$PROCSTRING" ]
 fi
 
 PROCSTRING_BASENAME=`basename ${PROCSTRING}`
-OUTPUT_FILER=/home/${USER}/tmpoutputs_adv/${PROCSTRING_BASENAME}.${HOSTNAME}.$$.output
+OUTPUT_FILER=/home/${USER}/tmpoutputs_no_adv/${PROCSTRING_BASENAME}.${HOSTNAME}.$$.output
 
 cd /home/lerrelp/rllab/adversary_scripts
 
 #run a niced python script 
-nice python train_trpo_var_adversary.py --env $env --adv_name adv --n_exps 3 --n_itr 500 --layer_size 64 64 --batch_size 25000 --if_render 0 --adv_fraction $adv_fraction > $OUTPUT_FILER
+nice python train_trpo_var_adversary.py --env $env --adv_name no_adv --n_exps 3 --n_itr 500 --layer_size 64 64 --batch_size 25000 --if_render 0 --adv_fraction $adv_fraction > $OUTPUT_FILER
 echo "Finished Without Problems" >> $OUTPUT_FILER
 echo "..::RL Solved::.."
