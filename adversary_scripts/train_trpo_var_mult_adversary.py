860e962a212988a9a48cf8f56379a7fa1268f264
lerrelpinto <lerrelp@yoda.graphics.cs.cmu.edu>
2017-01-17 21:41:26 +0000

    .

--- adversary_scripts/train_trpo_var_mult_adversary.py
+++ adversary_scripts/train_trpo_var_mult_adversary.py
@@ -5,6 +5,7 @@ from rllab.envs.normalized_env import normalize
 from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
 from rllab.policies.random_uniform_control_policy import RandomUniformControlPolicy
 from rllab.policies.constant_control_policy import ConstantControlPolicy
+from rllab.policies.step_control_policy import StepControlPolicy
 from IPython import embed
 import matplotlib.pyplot as plt
 import rllab.misc.logger as logger
@@ -18,7 +19,7 @@ import gym
 ## Pass arguments ##
 parser = argparse.ArgumentParser()
 parser.add_argument('--env', type=str, required=True, help='Name of adversarial environment')
-parser.add_argument('--adv_name', type=str, default='no_adv', help='DOESNT MATTER FOR THIS BASELINE')
+parser.add_argument('--adv_name', type=str, default='adv', help='adv if training with adversary, no_adv if training without adversary')
 parser.add_argument('--path_length', type=int, default=1000, help='maximum episode length')
 parser.add_argument('--layer_size', nargs='+', type=int, default=[100,100,100], help='layer definition')
 parser.add_argument('--if_render', type=int, default=0, help='Should we animate at all?')
@@ -31,7 +32,7 @@ parser.add_argument('--batch_size', type=int, default=4000, help='')
 parser.add_argument('--filename_append', type=str, default='', help='stuff to append to save filename')
 parser.add_argument('--save_every', type=int, default=100, help='')
 parser.add_argument('--n_process', type=int, default=16, help='Number of threads for sampling environment')
-parser.add_argument('--adv_fraction', type=float, default=1.0, help='DOESNT MATTER')
+parser.add_argument('--adv_fraction', type=float, default=1.0, help='fraction of maximum adversarial force to be applied')
 parser.add_argument('--step_size', type=float, default=0.01, help='step size for learner')
 parser.add_argument('--gae_lambda', type=float, default=0.97, help='gae_lambda for learner')
 
@@ -39,7 +40,7 @@ args = parser.parse_args()
 
 ## Number of experiments to run ##
 env_name = args.env
-adv_name = 'no_adv'
+adv_name = args.adv_name
 path_length = args.path_length
 layer_size = tuple(args.layer_size)
 ifRender = bool(args.if_render)
@@ -51,7 +52,7 @@ n_adv_itr = args.n_adv_itr
 batch_size = args.batch_size
 save_every = args.save_every
 n_process = args.n_process
-adv_fraction = 1.0
+adv_fraction = args.adv_fraction
 step_size = args.step_size
 gae_lambda = args.gae_lambda
 
@@ -60,8 +61,8 @@ rand_test_rew_summary = []
 step_test_rew_summary = []
 rand_step_test_rew_summary = []
 adv_test_rew_summary = []
-save_prefix = 'BASELINE-env-{}_{}_Exp{}_Itr{}_BS{}_Adv{}_stp{}_lam{}'.format(env_name, adv_name, n_exps, n_itr, batch_size, adv_fraction, step_size, gae_lambda)
-save_dir = os.environ['HOME']+'/results/baselines'
+save_prefix = 'env-{}_{}_Exp{}_Itr{}_BS{}_Adv{}_stp{}_lam{}'.format(env_name, adv_name, n_exps, n_itr, batch_size, adv_fraction, step_size, gae_lambda)
+save_dir = os.environ['HOME']+'/results/variable_adversary'
 fig_dir = 'figs'
 save_name = save_dir+'/'+save_prefix+'.p'
 fig_name = fig_dir+'/'+save_prefix+'.png'
@@ -77,23 +78,60 @@ for ne in range(n_exps):
     )
     pro_baseline = LinearFeatureBaseline(env_spec=env.spec)
 
+    ## ADVERSARY POLICIES TO CONSIDER ##
+
     ## Zero Adversary for the protagonist training ##
     zero_adv_policy = ConstantControlPolicy(
         env_spec=env.spec,
         is_protagonist=False,
         constant_val = 0.0
     )
+    ## Random Adversary for the protagonist training ##
+    rand_adv_policy = RandomUniformControlPolicy(
+        env_spec=env.spec,
+        is_protagonist=False,
+    )
+    ## Rand Step Adversary for the protagonist training ##
+    characteristic_length = path_length/5
+    step = path_length/10
+    rand_step_adv_policy = StepControlPolicy(
+        env_spec=env.spec,
+        characteristic_length=characteristic_length,
+        step_size=step,
+        is_random_mag=True,
+        is_protagonist=False,
+    )
+    ## Step Adversary for the protagonist training ##
+    characteristic_length = path_length/5
+    step = path_length/10
+    step_adv_policy = StepControlPolicy(
+        env_spec=env.spec,
+        characteristic_length=characteristic_length,
+        step_size=step,
+        is_random_mag=False,
+        is_protagonist=False,
+    )
+    ## Adversary policy definition ##
+    adv_policy = GaussianMLPPolicy(
+        env_spec=env.spec,
+        hidden_sizes=layer_size,
+        is_protagonist=False
+    )
+    protagonist_adversary_list = [zero_adv_policy, rand_adv_policy, rand_step_adv_policy, step_adv_policy, adv_policy] 
+ 
+    adv_baseline = LinearFeatureBaseline(env_spec=env.spec)
 
     ## Optimizer for the Protagonist ##
     from rllab.sampler import parallel_sampler
     parallel_sampler.initialize(n_process)
-    if adv_name=='no_adv':
+    protagonist_algos = []
+    for pro_adv in protagonist_adversary_list:
         pro_algo = TRPO(
             env=env,
             pro_policy=pro_policy,
-            adv_policy=zero_adv_policy,
+            adv_policy=pro_adv,
             pro_baseline=pro_baseline,
-            adv_baseline=pro_baseline,
+            adv_baseline=adv_baseline,
             batch_size=batch_size,
             max_path_length=path_length,
             n_itr=n_pro_itr,
@@ -102,6 +140,24 @@ for ne in range(n_exps):
             step_size=step_size,
             is_protagonist=True
         )
+        protagonist_algos.append(pro_algo)
+
+    ## Optimizer for the Adversary ##
+    adv_algo = TRPO(
+        env=env,
+        pro_policy=pro_policy,
+        adv_policy=adv_policy,
+        pro_baseline=pro_baseline,
+        adv_baseline=adv_baseline,
+        batch_size=batch_size,
+        max_path_length=path_length,
+        n_itr=n_adv_itr,
+        discount=0.995,
+        gae_lambda=gae_lambda,
+        step_size=step_size,
+        is_protagonist=False,
+        scope='adversary_optim'
+    )
 
     ## Joint optimization ##
     if ifRender==True: test_const_adv(env, pro_policy, path_length=path_length, n_traj = 1, render=True)
@@ -117,22 +173,32 @@ for ne in range(n_exps):
     rand_step_testing_rews = []
     rand_step_testing_rews.append(test_rand_step_adv(env, pro_policy, path_length=path_length))
     adv_testing_rews = []
-    adv_testing_rews.append(test_rand_adv(env, pro_policy, path_length=path_length))
-    #embed()
+    adv_testing_rews.append(test_learnt_adv(env, pro_policy, adv_policy, path_length=path_length))
+
+    algo_looper = 0
+    embed()
     for ni in range(n_itr):
+        pro_algo = protagonist_algos[algo_looper]
+        algo_looper = (algo_looper+1)%len(protagonist_algos)
         logger.log('\n\n\n####expNO{}_{} global itr# {}####\n\n\n'.format(ne,adv_name,ni))
         pro_algo.train()
         pro_rews += pro_algo.rews; all_rews += pro_algo.rews;
         logger.log('Protag Reward: {}'.format(np.array(pro_algo.rews).mean()))
+        adv_algo.train()
+        adv_rews += adv_algo.rews; all_rews += adv_algo.rews;
+        logger.log('Advers Reward: {}'.format(np.array(adv_algo.rews).mean()))
         const_testing_rews.append(test_const_adv(env, pro_policy, path_length=path_length))
         rand_testing_rews.append(test_rand_adv(env, pro_policy, path_length=path_length))
         step_testing_rews.append(test_step_adv(env, pro_policy, path_length=path_length))
         rand_step_testing_rews.append(test_rand_step_adv(env, pro_policy, path_length=path_length))
-        adv_testing_rews.append(test_rand_adv(env, pro_policy, path_length=path_length))
+        adv_testing_rews.append(test_learnt_adv(env, pro_policy, adv_policy, path_length=path_length))
+        if ni%afterRender==0 and ifRender==True:
+            test_const_adv(env, pro_policy, path_length=path_length, n_traj=1, render=True);
         if ni!=0 and ni%save_every==0:
             ## SAVING INFO ##
             pickle.dump({'args': args,
                          'pro_policy': pro_policy,
+                         'adv_policy': adv_policy,
                          'zero_test': const_test_rew_summary,
                          'rand_test': rand_test_rew_summary,
                          'step_test': step_test_rew_summary,
@@ -143,6 +209,7 @@ for ne in range(n_exps):
 
     ## Shutting down the optimizer ##
     pro_algo.shutdown_worker()
+    adv_algo.shutdown_worker()
     const_test_rew_summary.append(const_testing_rews)
     rand_test_rew_summary.append(rand_testing_rews)
     step_test_rew_summary.append(step_testing_rews)
@@ -152,6 +219,7 @@ for ne in range(n_exps):
 ## SAVING INFO ##
 pickle.dump({'args': args,
              'pro_policy': pro_policy,
+             'adv_policy': adv_policy,
              'zero_test': const_test_rew_summary,
              'rand_test': rand_test_rew_summary,
              'step_test': step_test_rew_summary,
